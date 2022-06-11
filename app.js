46e8905e6292cb1bd927b1043d688bce5c9df6b3
themindasrimal <themindasrimal@gmail.com>
2022-06-11 17:43:49 +0530

    first

---
+++ app.js
@@ -0,0 +1,66 @@
+// @ts-check
+//  <ImportConfiguration>
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+const config = require("./config");
+const dbContext = require("./data/databaseContext");
+//  </ImportConfiguration>
+
+//  <DefineNewItem>
+const newItem = {
+  id: "31",
+  category: "fun",
+  name: "Cosmos DB",
+  description: "Complete Cosmos DB Node.js Quickstart ⚡",
+  isComplete: false
+};
+//  </DefineNewItem>
+
+async function main() {
+  
+  // <CreateClientObjectDatabaseContainer>
+  const { endpoint, key, databaseId, containerId } = config;
+
+  const client = new CosmosClient({ endpoint, key });
+
+  const database = client.database(databaseId);
+  const container = database.container(containerId);
+
+  // Make sure Tasks database is already setup. If not, create it.
+  await dbContext.create(client, databaseId, containerId);
+  // </CreateClientObjectDatabaseContainer>
+  
+  try {
+    // <QueryItems>
+    console.log(`Querying container: Items`);
+
+    // query to return all items
+    const querySpec = {
+      query: "SELECT * from c"
+    };
+    
+    // read all items in the Items container
+    const { resources: items } = await container.items
+      .query(querySpec)
+      .fetchAll();
+
+    items.forEach(item => {
+      console.log(`${item.id} - ${item.description}`);
+    });
+    // </QueryItems>
+    
+    // <CreateItem>
+    /** Create new item
+     * newItem is defined at the top of this file
+     */
+    const { resource: createdItem } = await container.items.create(newItem);
+    
+    console.log(`\r\nCreated new item: ${createdItem?.id} - ${createdItem?.description}\r\n`);
+    // </CreateItem>
+    
+    
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+main();
