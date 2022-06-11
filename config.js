4a4d729fcd139cc6b1ab45de8601602894b1b34e
Theminda Srimal <67902627+ThemindaSrimal@users.noreply.github.com>
2022-06-11 21:57:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 // @ts-check
 
 const config = {
-    endpoint: "https://javascriptexamplets.documents.azure.com:443/",
-    key: "JoM7tkL0Kn6rU2x4Yw7z3XWIUqZ0FWiZeXxn4TVuA0xJNnNpDXnRq9FbCJ8W1orL0TnEwIW1UvkOANHCR7Ljxw==",
+    
     databaseId: "Tasks",
     containerId: "Items",
     partitionKey: { kind: "Hash", paths: ["/category"] }
