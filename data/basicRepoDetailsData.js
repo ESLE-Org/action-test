ca950dd926d6f2b75ec2eded4065f5f34ccb937e
Theminda Srimal <67902627+ThemindaSrimal@users.noreply.github.com>
2022-06-13 17:39:47 +0530

    Update basicRepoDetailsData.js

--- data/basicRepoDetailsData.js
+++ data/basicRepoDetailsData.js
@@ -5,7 +5,7 @@ const basicRepoDetailsData = {
     orgId: "abcdMDEyOk9yZ2FuaXphdGlvbjYxNTQ3MjI=",
     repoName: "HealthVault-Mobile-iOS-Library",
     repoWatchStatus: 0,
-    tag: "Not Specified now",
+    tag: "Not Specified currently",
     id: "abcdMDEwOlJlcG9zaXRvcnkxOTMyMDgz"
 };
 
