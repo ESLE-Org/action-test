b8bfd8393a6e333395ea49533926f6e2dab4d8c1
Theminda Srimal <67902627+ThemindaSrimal@users.noreply.github.com>
2022-06-13 14:13:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 // @ts-check
-require('dotenv').config();
 
 const config = {
-    endpoint: process.env.ENDPOINT,
-    key: process.env.KEY,
     databaseId: "Tasks",
     containerId: "Items",
     partitionKey: { kind: "Hash", paths: ["/category"] }
