7cd8c128ef91ad20fe3ccbfb04399ee6e2ce70bd
themindasrimal <themindasrimal@gmail.com>
2022-06-13 14:59:10 +0530

    4 items

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const config = require("./config");
 const dbContext = require("./data/databaseContext");
 //  </ImportConfiguration>
 
-//  <DefineNewItem>
-const newItem = {
+
+const BasicRepoDetails = {
   createdAt: "2011-06-21T22:49:39Z",
   monitorStatus: 0,
   orgId: "MDEyOk9yZ2FuaXphdGlvbjYxNTQ3MjI=",
@@ -17,7 +17,63 @@ const newItem = {
   tag: "Not Specified",
   id: "MDEwOlJlcG9zaXRvcnkxOTMyMDgz"
 };
-//  </DefineNewItem>
+
+const Languages = {
+  name: "Objective-C",
+  orgId: "MDEyOk9yZ2FuaXphdGlvbjYxNTQ3MjI=",
+  testingTools: [],
+  id: "MDg6TGFuZ3VhZ2UxNTY="
+};
+
+const Repositories = {
+  description: "The HealthVault team has recently added the capability to write applications that will run on Mobile Devices and connect directly to the HealthVault Service. HealthVault iOS library enables developers to write iOS applications which interact with a user's personally controlled health record. The â€œgo liveâ€� process to deploy an application against the production HealthVault service can be found at http://msdn.microsoft.com/en-us/healthvault/bb962148. Information about the API that the HealthVault service exposes can be in the documentation in HealthVault .NET SDK.",
+  dbUpdatedAt: "2022-06-11T18:18:55.161152200Z",
+  languages: [
+      {
+          "name": "Objective-C",
+          "id": "MDg6TGFuZ3VhZ2UxNTY=",
+          "color": "#438eff"
+      },
+      {
+          "name": "C",
+          "id": "MDg6TGFuZ3VhZ2UxNDk=",
+          "color": "#555555"
+      }
+  ],
+  monitorStatus: 0,
+  openPRs: [
+      {
+          "prNumber": 3,
+          "prUrl": "https://github.com/microsoft/HealthVault-Mobile-iOS-Library/pull/3",
+          "lastCommit": {
+              "commitUrl": "https://github.com/microsoft/HealthVault-Mobile-iOS-Library/commit/ce8b99d46e34fe8ed517f8ec3efa8df793bfec4b",
+              "oid": "ce8b99d46e34fe8ed517f8ec3efa8df793bfec4b",
+              "status": {
+                  "state": "SUCCESS",
+                  "contexts": [
+                      {
+                          "context": "license/cla",
+                          "state": "SUCCESS",
+                          "targetUrl": null,
+                          "description": "All CLA requirements met."
+                      }
+                  ]
+              }
+          }
+      }
+  ],
+  orgId: "MDEyOk9yZ2FuaXphdGlvbjYxNTQ3MjI=",
+  repoName: "HealthVault-Mobile-iOS-Library",
+  repoUrl: "https://github.com/microsoft/HealthVault-Mobile-iOS-Library",
+  updatedAt: "2021-12-07T16:39:05Z",
+  id: "MDEwOlJlcG9zaXRvcnkxOTMyMDgz"
+};
+
+const Tags = {
+  tag: "Not Specified",
+  orgId: "MDEyOk9yZ2FuaXphdGlvbjYxNTQ3MjI=",
+  id: "Not Specified"
+};
 
 async function main() {
   
@@ -34,31 +90,16 @@ async function main() {
   // </CreateClientObjectDatabaseContainer>
   
   try {
-    // <QueryItems>
-    console.log(`Querying container: Items`);
-
-    // query to return all items
-    const querySpec = {
-      query: "SELECT * from c"
-    };
-    
-    // read all items in the Items container
-    const { resources: items } = await container.items
-      .query(querySpec)
-      .fetchAll();
-
-    items.forEach(item => {
-      console.log(`${item.id} - ${item.description}`);
-    });
-    // </QueryItems>
     
     // <CreateItem>
-    /** Create new item
-     * newItem is defined at the top of this file
+    /** Create new items
+     * newitems are defined at the top of this file
      */
-    const { resource: createdItem } = await container.items.create(newItem);
-    
-    console.log(`\r\nCreated new item: ${createdItem?.id} - ${createdItem?.description}\r\n`);
+    await container.items.create(BasicRepoDetails);
+    await container.items.create(Languages);
+    await container.items.create(Repositories);
+    await container.items.create(Tags);
+
     // </CreateItem>
     
     
